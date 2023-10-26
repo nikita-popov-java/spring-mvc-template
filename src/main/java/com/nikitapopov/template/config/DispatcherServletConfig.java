9e947ead5c0084220d7c77c36b75958572d42516
nikita <nikitapopov1295@gmail.com>
2023-10-26 12:28:52 +0500

    init

--- src/main/java/com/nikitapopov/template/config/DispatcherServletConfig.java
+++ src/main/java/com/nikitapopov/template/config/DispatcherServletConfig.java
@@ -1,8 +1,11 @@
 package com.nikitapopov.template.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class CustomDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
@@ -16,5 +19,16 @@ public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherS
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
+                .addMappingForUrlPatterns(null ,true, "/*");
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
+<head>
+    <title>Main page</title>
+</head>
 <body>
 <h2>Hello World!</h2>
+<a href="${pageContext.request.contextPath}/people">Let's go to watching people!</a>
 </body>
 </html>
