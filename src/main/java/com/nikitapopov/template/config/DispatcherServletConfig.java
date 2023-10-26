1afbada1f0f4cbf7802e96a98a5e1a595894835c
nikita <nikitapopov1295@gmail.com>
2023-10-26 19:13:45 +0500

    init

--- src/main/java/com/nikitapopov/template/config/DispatcherServletConfig.java
+++ src/main/java/com/nikitapopov/template/config/DispatcherServletConfig.java
@@ -5,7 +5,7 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class CustomDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
